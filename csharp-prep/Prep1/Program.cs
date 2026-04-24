7669443458ea35660df0f6546b5f2650f68b6189
wattsk95 <wat21035@byui.edu>
2026-04-24 13:03:25 -0600

    added the top things from the example code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,6 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
 Console.Write("What is your first name? ");
 string fname = Console.ReadLine();
 Console.Write("What is your last name? ");
 string lname = Console.ReadLine();
 
 Console.WriteLine($"Your name is {lname}, {fname} {lname}. ");
+
+    }
+
+}
