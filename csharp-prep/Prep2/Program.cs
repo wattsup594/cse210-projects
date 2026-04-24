776f0ef5de389c97dde8ca255b3dad64b73ae51b
wattsk95 <wat21035@byui.edu>
2026-04-24 13:21:29 -0600

    added the grade conditonal practice to Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}. ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the course! ");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time! ");
+        }
     }
 }
